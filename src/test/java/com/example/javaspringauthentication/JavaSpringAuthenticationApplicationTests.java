7df4fab5adedf2348e38acdee26b986517e16003
Priyal Patney <priyal_patney@hotmail.com>
2020-12-31 20:46:24 +0530

    Initial spring setup

---
+++ src/test/java/com/example/javaspringauthentication/JavaSpringAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
